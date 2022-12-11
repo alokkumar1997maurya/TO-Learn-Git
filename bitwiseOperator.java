41c61b63189069f5901932e8ddffcbe89fbc4a9f
alok <alokkumar1997maurya@gmail.com>
2022-12-11 19:07:43 +0530

    bit wise operator fine

--- bitwiseOperator.java
+++ bitwiseOperator.java
@@ -1,5 +1,53 @@
 class bitwiseOperator{
     public static void main(String[] args) {
-        System.out.println( 5&6);
+        System.out.println( 5&6);   // AND operatior 
+        System.out.println(9|7);     // OR operator
+        
+        
+        
+        
+        
+        System.out.println(5^6);        //  XOR operator
+
+        /* 
+             XOR operator how to work 
+             0 ^ 0 => 0
+             1 ^ 0 => 1
+             0 ^ 1 => 1
+             1 ^ 1 => 0
+
+             it conclusion for different value it give 1 value and for same value it give  0 . 
+         */
+
+
+
+            System.out.println(~-1);  // Binary one's compliment  (not operator )
+
+            
+
+
+
+            System.out.println(4<<1); // Binary Left Shift  ( << )  
+
+                /*   formula of  binary left shift  
+                 *      
+                 *     formula ==> [  a<<b = a*2^b ]
+                 * 
+                 */
+        
+               
+                     System.out.println(4>>1);  // Right Shift ( >> )
+
+                     /*
+                            formula of the right shift 
+                            
+                            formula  ==>  
+
+
+                      */
+
+
+
+
     }
 }
