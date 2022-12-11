14c65b305f45468bb65d72d12a6fc571b95735ac
alok <alokkumar1997maurya@gmail.com>
2022-12-11 23:23:03 +0530

    updatevalue

--- setIthvalueAtPosition.java
+++ setIthvalueAtPosition.java
@@ -9,10 +9,19 @@ public class setIthvalueAtPosition {
         int bitValue = ~(1<<i);
         return n & bitValue;
      }
+      public static int updatevalue(int n, int i, int setbit) {
+        if(setbit==0){
+           
+            return clearValue(n, i);
+        }else
+        return SetValue(n, i);
+      }
 
     public static void main(String[] args) {
          System.out.println("Set the value given position:"+SetValue(2, 0));
 
         System.out.println("Clear the value to given position:"+clearValue(10,1));
+
+        System.out.println(updatevalue(4, 0, 1));
     }
 }
